aaff215dde018865b52e1f685797951e66f2de6f
haxy <zaidsaheb4@gmail.com>
2024-02-21 00:48:32 +0100

    added start of election

--- hardhat/contracts/voting.sol
+++ hardhat/contracts/voting.sol
@@ -23,6 +23,46 @@ contract Voting {
   uint public endTime;
 
   // Voting status
-  bool public votingStatus;
+  bool public electionStatus;
+
+
+  // Voting Permissions
+  modifier onlyOwner() {
+    require(msg.sender == owner, "Only owner can call this function");
+    _;
+  }
+
+
+  // Check voting status
+  modifier votingOngoing() {
+    require(electionStatus, "Voting is not started yet");
+    _;
+  }
+
+  // Constructor
+  constructor() {
+    owner = msg.sender;
+
+  }
+
+  // Start voting
+  function startVoting(string[] memory _candidates, uint256 _votingDuration) public onlyOwner {
+    require(electionStatus == false, "Voting is already started"); // Check if voting is already started
+    delete candidates;
+    // resetAllVoterStatus();
+
+    for (uint i = 0; i < _candidates.length; i++) {
+      candidates.push(Candidate({
+        id: i,
+        name: _candidates[i],
+        voteCount: 0}));
+    }
+    electionStatus = true;
+    startTime = block.timestamp;
+    endTime = block.timestamp + (_votingDuration * 1 minutes);
+  }
+
+
+  //add candidate
 
 }
