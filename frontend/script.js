6601e9344a929eae1bb69921cf39e9d8c815174c
haxy <zaidsaheb4@gmail.com>
2024-02-21 16:29:32 +0100

    added document selectors

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,10 @@
-connectWalletMsg = document.getElementById('connectWalletMsg');
-connectWalletBtn = document.getElementById('connectWalletBtn');
-votingStation = document.getElementById('votingStation');
-timerTime = document.getElementById('time');
+const connectWalletMsg = document.getElementById('#connectWalletMsg');
+const connectWalletBtn = document.getElementById('#connectWalletBtn');
+const votingStation = document.getElementById('#votingStation');
+const timerTime = document.getElementById('#time');
+const timerMessage = document.getElementById('#timerMessage');
+const mainBoard = document.getElementById('#mainBoard');
+const voteForm = document.getElementById('#voteForm');
+const vote = document.getElementById('#vote');
+const voteBtn = document.getElementById('#sendVote');
+
