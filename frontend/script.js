374db13bdb881cb8332e3e04b256a5f81b490335
haxy <zaidsaheb4@gmail.com>
2024-02-21 16:23:16 +0100

    started javascript

---
+++ frontend/script.js
@@ -0,0 +1,4 @@
+connectWalletMsg = document.getElementById('connectWalletMsg');
+connectWalletBtn = document.getElementById('connectWalletBtn');
+votingStation = document.getElementById('votingStation');
+timerTime = document.getElementById('time');
